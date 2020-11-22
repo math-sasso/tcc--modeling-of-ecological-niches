99c8cc5a1140fce637c2004a3867beaad9251d11
Matheus Sasso <matheus.sasso17@gmail.com>
2020-11-22 12:48:52 -0300

    adding self to utils.py

--- samples/files_for_wget/utils.py
+++ samples/files_for_wget/utils.py
@@ -5,18 +5,18 @@ class Utils:
     def __init__(self):
         pass
         
-    def retrieve_data_from_np_array(path):
+    def retrieve_data_from_np_array(self,path):
         """ Read a numpy array"""
         with open(path, 'rb') as f:
             np_array = np.load(f)
         return np_array
 
-    def create_folder_structure(folder):
+    def create_folder_structure(self,folder):
         """ Create the comple folder structure if it does not exists """
         if not os.path.exists(folder):
             os.makedirs(folder)
 
-    def save_nparray_to_folder(np_array,folder_path,filename):
+    def save_nparray_to_folder(self,np_array,folder_path,filename):
         """ Save numpy array to the specified folder path """
         complete_path = os.path.join(folder_path,filename+'.npy')
         with open(complete_path, 'wb') as f:
