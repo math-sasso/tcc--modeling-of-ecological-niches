be9e19c9433a45796ee90b214e171c1fdcb47543
Matheus Gustavo Alves Sasso <msasso@cpqd.com.br>
2020-12-04 17:08:54 -0300

    changing df_ŕesences reading

--- samples/files_for_wget/brazil.py
+++ samples/files_for_wget/brazil.py
@@ -172,8 +172,9 @@ class Brazil:
 
     if ".csv" in species_presences_path:
       df_species_presences = pd.read_csv(species_presences_path)
-      df_species_presences['geometry'] = df_species_presences['geometry'].apply(wkt.loads)
-      gdf_species_presences = gpd.GeoDataFrame(df_species_presences, crs='epsg:4326')
+      gdf_species_presences = gpd.GeoDataFrame(df_species_presences, geometry=gpd.points_from_xy(df_species_presences.LONGITUDE, df_species_presences.LATITUDE),crs='epsg:4326')
+      # df_species_presences['geometry'] = df_species_presences['geometry'].apply(wkt.loads)
+      # gdf_species_presences = gpd.GeoDataFrame(df_species_presences, crs='epsg:4326')
     elif ".shp" in species_presences_path:
       gdf_species_presences = gpd.read_file(species_presences_path)
     gdf_species_presences.plot(ax=ax, color='blue', markersize=5,label="presences")
@@ -181,7 +182,7 @@ class Brazil:
     if species_absences_path:
       if ".csv" in species_absences_path:
         df_species_absences = pd.read_csv(species_absences_path)
-        gdf_species_absences = gpd.GeoDataFrame(df_species_absences, geometry=gpd.points_from_xy(df_species_absences.LONGITUDE, df_species_absences.LATITUDE))
+        gdf_species_absences = gpd.GeoDataFrame(df_species_absences, geometry=gpd.points_from_xy(df_species_absences.LONGITUDE, df_species_absences.LATITUDE),crs='epsg:4326')
       elif ".shp" in species_absences_path:
         gdf_species_absences = gpd.read_file(species_absences_path)
       gdf_species_absences.plot(ax=ax, color='red', markersize=5,label="absences")
